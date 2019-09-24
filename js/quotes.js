942bd717d916ff2aa73cf12c933aaeac4f3e8437
mhxander <55026094+mhxander@users.noreply.github.com>
2019-09-24 08:29:52 -0400

    Added Initial project files

---
+++ js/quotes.js
@@ -0,0 +1,74 @@
+var quotes = [ 
+  { 
+    quote: 'Luke, you’re going to find that many of the truths we cling to depend greatly on our own point of view.',
+    source: 'Obi Wan Kenobi',
+    actor: 'Sir Alec Guinness',
+    movie: 'Star Wars: Episode VI- Return of the Jedi',
+    year: 1983
+  },
+  {
+    quote: 'Every man dies, but not every man really lives.',
+    source: 'William Wallace',
+    actor: 'Mel Gibson',
+    movie: 'Braveheart',
+    year: 1995
+  },
+  {
+    quote: 'Life moves pretty fast. If you don’t stop and look around once in a while, you could miss it.',
+    source: 'Ferris Bueller',
+    actor: 'Matthew Broderick',
+    movie: "Ferris Bueller's Day Off",
+    year: 1990
+  },
+  {
+    quote: 'Strange, isn’t it? Each man’s life touches so many other lives. When he isn’t around he leaves an awful hole, doesn’t he?',
+    source: 'Clarence',
+    actor: 'Henry Travers',
+    movie: "It's A Wonderful Life",
+    year: 1947
+  },
+  {
+    quote: 'All we have to decide is what to do with the time that is given to us.',
+    source: 'Gandalf',
+    actor: 'Ian McKellen',
+    movie: "The Lord of the Rings: The Fellowship of the Ring",
+    year: 2001
+  },
+  {
+    quote: 'Kid, there are heroes and there are legends. Heroes get remembered, but legends never die. Follow your heart, kid, and you’ll never go wrong.',
+    source: 'Babe Ruth',
+    actor: 'Art LaFleur',
+    movie: "The Sandlot",
+    year: 1993
+  },
+  {
+    quote: 'Oh yes, the past can hurt. But you can either run from it, or learn from it.',
+    source: 'Rafiki',
+    actor: 'Robert Guillaume',
+    movie: "The Lion King",
+    year: 1994
+  },
+  {
+    quote: "That there's some good in this world, Mr. Frodo... and it's worth fighting for.",
+    source: 'Samwise Gamgee',
+    actor: 'Sean Astin',
+    movie: "The Lord of the Rings: The Two Towers",
+    year: 2002
+  },
+  {
+    quote: 'Whoever saves one life, saves the world entire.',
+    source: 'Itzhak Stern',
+    actor: 'Ben Kingsley',
+    movie: "Schindler's List",
+    year: 1994
+  },
+  {
+    quote: 'We tolds him to go away, and away he goes, Precious! Gone! Gone! Gone! Smeagol is free!',
+    source: 'Smeagol/Gollum',
+    actor: 'Andy Serkis',
+    movie: "The Lord of the Rings: The Two Towers",
+    year: 2002
+  }
+];
+
+
